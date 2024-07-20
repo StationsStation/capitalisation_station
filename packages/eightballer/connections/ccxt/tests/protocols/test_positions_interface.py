b0a593c017bd2dfb384b9e7db16d621d9b1e3ece
8baller <tomrae030@hotmail.com>
2024-07-20 08:54:27 +0100

    feat: added tests

---
+++ packages/eightballer/connections/ccxt/tests/protocols/test_positions_interface.py
@@ -0,0 +1,68 @@
+# ./test_position.py
+
+# Path: tests/test_connections/test_ccxt_connection/protocols/test_position_interface.py
+"""
+Tests for the position interface.
+"""
+import asyncio
+from unittest.mock import patch
+
+import pytest
+from aea.mail.base import Envelope
+
+from packages.ardian.connections.ccxt.interfaces.interface_base import get_dialogues
+from packages.eightballer.protocols.positions.custom_types import Positions
+from packages.eightballer.protocols.positions.dialogues import (
+    BasePositionsDialogues,
+    PositionsDialogue,
+)
+from packages.eightballer.protocols.positions.message import PositionsMessage
+from tests.test_connections.test_ccxt_connection.test_ccxt_connection import (
+    BaseCcxtConnectionTest,
+    get_dialogues,
+    with_timeout,
+)
+
+
+@pytest.mark.asyncio
+class TestPositionInterface(BaseCcxtConnectionTest):
+    """
+    Test the position interface.
+    """
+
+    DIALOGUES = get_dialogues(BasePositionsDialogues, PositionsDialogue)
+
+    @pytest.mark.asyncio
+    @with_timeout(4)
+    async def test_get_all_positions(self):
+        """
+        Test the get all positions method.
+        """
+        await self.connection.connect()
+        dialogues = self.DIALOGUES(self.client_skill_id)  # pylint: disable=E1120
+        request, _ = dialogues.create(
+            counterparty=str(self.connection.connection_id),
+            performative=PositionsMessage.Performative.GET_ALL_POSITIONS,
+            exchange_id="deribit",
+            params={},
+        )
+        envelope = Envelope(
+            to=request.to,
+            sender=request.sender,
+            message=request,
+        )
+        # we make sure that we mock the api call
+        with patch.object(
+            self.connection._exchanges["deribit"],
+            "fetch_positions",
+            return_value=[],
+        ) as mock_get_all_positions:
+            await self.connection.send(envelope)
+            await asyncio.sleep(1)
+            response = await self.connection.receive()
+        assert response is not None
+        assert isinstance(response.message, PositionsMessage)
+        assert (
+            response.message.performative == PositionsMessage.Performative.ALL_POSITIONS
+        ), "Error: {}".format(response.message)
+        assert isinstance(response.message.positions, Positions)
