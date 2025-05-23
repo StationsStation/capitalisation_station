17b5854c79d4187e2e0d5172566dd28671789025
8ball030 <8baller@station.codes>
2025-05-23 17:14:28 +0100

    syncing:hashes

--- packages/eightballer/connections/dcxt/interfaces/asset_bridging.py
+++ packages/eightballer/connections/dcxt/interfaces/asset_bridging.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """Interface for asset_bridging protocol."""
 
 from typing import TYPE_CHECKING
@@ -21,6 +22,21 @@ if TYPE_CHECKING:
 ErrorCode = AssetBridgingMessage.ErrorInfo.Code
 
 
+=======
+import requests
+
+from derive_client.data_types import ChainID, Currency
+from derive_client.clients.async_client import AsyncClient
+
+from packages.eightballer.connections.dcxt import dcxt
+from packages.eightballer.connections.dcxt.dcxt.derive import DeriveClient
+from packages.zarathustra.protocols.asset_bridging.custom_types import BridgeStatus
+from packages.zarathustra.protocols.asset_bridging.message import AssetBridgingMessage
+from packages.zarathustra.protocols.asset_bridging.dialogues import AssetBridgingDialogue, BaseAssetBridgingDialogue
+from packages.eightballer.connections.dcxt.interfaces.interface_base import BaseInterface
+
+
+>>>>>>> def61c3 (feat: AssetBridgingInterface draft)
 class AssetBridgingInterface(BaseInterface):
     """Interface for the asset bridging protocol."""
 
@@ -28,6 +44,7 @@ class AssetBridgingInterface(BaseInterface):
     dialogue_class = AssetBridgingDialogue
     dialogues_class = BaseAssetBridgingDialogues
 
+<<<<<<< HEAD
     async def request_bridge(  # noqa: PLR0911
         self, message: AssetBridgingMessage, dialogue: AssetBridgingDialogue, connection
     ) -> AssetBridgingMessage | None:
@@ -82,6 +99,42 @@ class AssetBridgingInterface(BaseInterface):
         if request.source_chain == ChainID.DERIVE.name:
             receiver = client.signer.address
             tx_result = client.withdraw_from_derive(
+=======
+    async def request_bridge(
+            self, message: AssetBridgingMessage, dialogue: AssetBridgingDialogue, connection
+        ) -> AssetBridgingMessage | None:
+
+        if not message.performative == AssetBridgingMessage.Performative.REQUEST_BRIDGE:
+            raise RuntimeError()
+
+        source_chain: str = message.source_chain
+        target_chain: str = message.target_chain
+        source_token: str = message.source_token
+        target_token: str | None = message.target_token
+        bridge: str = message.bridge
+        kwargs: dict[str, str] | None = message.kwargs
+
+        if not bridge == "derive":
+            raise NotImplementedError()
+        if not (source_chain == "derive" or target_chain == "derive"):
+            raise NotImplementedError()
+        if target_token and target_token != source_token:
+            raise NotImplementedError()
+        
+        ledger_id = exchange_id = bridge
+        exchange: DeriveClient = connection.exchanges[ledger_id][exchange_id]
+        client: AsyncClient = exchange.client
+
+        currency = Currency[source_token]
+        source_chain_id = ChainID[source_chain.upper()]
+        target_chain_id = ChainID[target_chain.upper()]
+
+        amount = 1  # TODO: update protocol performative
+
+        if source_chain == "derive":
+            receiver = client.signer.address
+            client.withdraw_from_derive(
+>>>>>>> def61c3 (feat: AssetBridgingInterface draft)
                 chain_id=target_chain_id,
                 currency=currency,
                 amount=amount,
@@ -89,13 +142,18 @@ class AssetBridgingInterface(BaseInterface):
             )
         else:
             receiver = client.wallet
+<<<<<<< HEAD
             tx_result = client.deposit_to_derive(
+=======
+            client.deposit_to_derive(
+>>>>>>> def61c3 (feat: AssetBridgingInterface draft)
                 chain_id=source_chain_id,
                 currency=currency,
                 amount=amount,
                 receiver=receiver,
             )
 
+<<<<<<< HEAD
         match tx_result.status:
             case TxStatus.FAILED:
                 status = BridgeResult.BridgeStatus.BRIDGE_STATUS_FAILED
@@ -193,3 +251,30 @@ class AssetBridgingInterface(BaseInterface):
                 message=err_msg,
             ),
         )
+=======
+        # TODO: 
+        # - obtain tx_hash (not currently returned by client)
+        # - obtain / determine status
+        tx_hash = b""
+        status = BridgeStatus(status=BridgeStatus.BridgeStatusEnum.BRIDGE_STATUS_ENUM_COMPLETED)
+
+        response_message = dialogue.reply(
+            performative=AssetBridgingMessage.Performative.BRIDGE_STATUS,
+            target_message=message,
+            status=status,
+            tx_hash=tx_hash,
+        )
+        return response_message
+
+    async def request_status(
+            self, message: AssetBridgingMessage, dialogue: AssetBridgingDialogue, connection
+        ) -> AssetBridgingMessage | None:
+        if not message.performative == AssetBridgingMessage.Performative.REQUEST_STATUS:
+            raise
+
+        tx_hash: bytes = message.tx_hash
+
+        # TODO: poll for receipt or check logs for TokensBridged event
+        # Protocol is atomic; we currently don't have context of original bridge request
+        # This orignal bridge request provide the context necessary to check for status
+>>>>>>> def61c3 (feat: AssetBridgingInterface draft)
