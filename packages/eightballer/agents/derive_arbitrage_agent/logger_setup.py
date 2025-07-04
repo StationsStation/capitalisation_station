511dd3c499a73c91449b4af389e212261916631b
zarathustra <zarathustra_git@pm.me>
2025-07-04 16:39:50 +0200

    feat: introduce json formatter for logfile

--- packages/eightballer/agents/derive_arbitrage_agent/logger_setup.py
+++ packages/eightballer/agents/derive_arbitrage_agent/logger_setup.py
@@ -2,6 +2,7 @@
 
 # ruff: noqa: D101, D102
 
+import json
 import logging
 
 
@@ -10,3 +11,17 @@ class ShortNameFormatter(logging.Formatter):
         parts = record.name.split(".")
         record.short_name = ".".join(parts[-3:])
         return super().format(record)
+
+
+class JSONFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        obj = {
+            "time": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "logger": record.name,
+            "message": record.getMessage(),
+            "name": record.name,
+            "pathname": record.pathname,
+            "line": record.lineno,
+        }
+        return json.dumps(obj, ensure_ascii=False)
