ce16bac8097ed41fb3df38a9a14991ace30a39af
Niels Launert <niels@blockwerft.de>
2025-07-08 16:02:35 +0200

    Add `NablaQuote` contract (returns bid/ask in single call)

---
+++ packages/dakavon/contracts/nabla_quote/contract.py
@@ -0,0 +1,41 @@
+"""This module contains the scaffold contract definition."""
+
+# ruff: noqa: PLR0904
+from aea.common import JSONLike
+from aea.crypto.base import Address, LedgerApi
+from aea.contracts.base import Contract
+from aea.configurations.base import PublicId
+
+
+class NablaQuote(Contract):
+    """The scaffold contract class for a smart contract."""
+
+    contract_id = PublicId.from_str("open_aea/scaffold:0.1.0")
+
+    @classmethod
+    def nabla_portal(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+    ) -> JSONLike:
+        """Handler method for the 'nabla_portal' requests."""
+        instance = cls.get_instance(ledger_api, contract_address)
+        result = instance.functions.nablaPortal().call()
+        return {"address": result}
+
+    @classmethod
+    def quote(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        amount_in: int,
+        token_path: list[Address],
+        router_path: list[Address],
+        token_prices: list[int],
+    ) -> JSONLike:
+        """Handler method for the 'quote' requests."""
+        instance = cls.get_instance(ledger_api, contract_address)
+        result = instance.functions.quote(
+            _amountIn=amount_in, _tokenPath=token_path, _routerPath=router_path, _tokenPrices=token_prices
+        ).call()
+        return {"bidAmountOut_": result, "askAmountOut_": result}
